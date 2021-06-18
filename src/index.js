d134603127b2d5206a6426fe68031c627787bd29
AlonaNihalchuk <acetnikopol@gmail.com>
2021-06-18 16:59:53 +0200

    libraryPage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './sass/main.scss';
+import libraryHeaderTpl from './templates/libraryHeaderTpl.hbs';
+
+const libraryPage = document.querySelector('.js-library');
+const pageHeader = document.querySelector('.page-header');
+
+libraryPage.addEventListener('click', onLibraryPageClick);
+
+function onLibraryPageClick(e) {
+  e.preventDefault();
+  const homeBgcContainer = document.getElementById('home');
+  home.remove();
+  pageHeader.insertAdjacentHTML('beforeend', libraryHeaderTpl());
+}
